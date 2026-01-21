70eebf84ecd781a7f62c5541b91c85a8c3ba1dec
AMAN PANDEY <man22invisible@gmail.com>
2026-01-21 20:24:00 +0530

    Initial Commit

---
+++ ZECer/ZECerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZECerApp.swift
+//  ZECer
+//
+//  Created by Aman Pandey on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ZECerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
